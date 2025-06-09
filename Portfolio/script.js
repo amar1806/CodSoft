84b3a9577f00067f880a22bea55219fd351bdf15
amar1806 <140941948+amar1806@users.noreply.github.com>
2025-06-09 18:29:33 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+// Dark mode toggle
+const toggleBtn = document.getElementById('darkToggle');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  toggleBtn.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
